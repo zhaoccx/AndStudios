516270994e5da2e630576c39216589b55ed7fe47
趙常春 <zhaoccx@hotmail.com>
2016-04-23 01:50:24 +0800

    Man

--- app/src/main/java/com/hall/andy/hall/MainActivity.java
+++ app/src/main/java/com/hall/andy/hall/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println("name");
         setContentView(R.layout.activity_main);
     }
 }
